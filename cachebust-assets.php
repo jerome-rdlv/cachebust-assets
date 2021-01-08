e583fe633a1975886fb15271fc63aeb30a70c730
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2021-01-08 13:57:09 +0100

    Add default filters to disable cachebust for /wp/ urls and when on admin or wp-login.php

--- cachebust-assets.php
+++ cachebust-assets.php
@@ -29,16 +29,15 @@ if (!class_exists('\Rdlv\WordPress\CacheBustAssets\BusterFactory')) {
 }
 
 add_action('init', function () {
-
     if (!apply_filters('cachebust_assets_enabled', !is_admin())) {
         return;
     }
-    
+
     // home path resolution
     $home_url = get_option('home');
     $site_url = get_option('siteurl');
     $home_path = (new WordPressRootPath())->get($home_url, $site_url, ABSPATH);
-    
+
     $factory = new BusterFactory($home_url, $home_path, function ($url) {
         return apply_filters('cachebust_url', true, $url);
     });
@@ -47,17 +46,24 @@ add_action('init', function () {
         apply_filters('cachebust_assets_mode', BusterFactory::MODE_QUERY_STRING)
     );
 
+    // default filters
+    add_filter('cachebust_url', function ($cachebust, $url) {
+        return strpos($url, '/wp/') === false;
+    }, 5, 2);
+    add_filter('cachebust_assets_enabled', function () {
+        return !is_admin() && $GLOBALS['pagenow'] !== 'wp-login.php';
+    });
+
     add_filter('script_loader_src', [$buster, 'cacheBustUrl']);
     add_filter('style_loader_src', [$buster, 'cacheBustUrl']);
 
     add_filter('wp_get_attachment_image_attributes', [$buster, 'cacheBustImageAttributes']);
     add_filter('post_thumbnail_html', [$buster, 'cacheBustThumbnail']);
     add_filter('wp_calculate_image_srcset', [$buster, 'cacheBustSrcset'], 10, 3);
-    
+
     add_filter('site_icon_meta_tags', [$buster, 'cacheBustFavicons']);
 
     // utilities
     add_filter('cache_bust_url', [$buster, 'cacheBustUrl']);
     add_filter('cache_bust_acf_image', [$buster, 'cacheBustAcfImage']);
-
 });
